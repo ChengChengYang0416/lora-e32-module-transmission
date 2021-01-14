28c14ca1f2102e2db2f5365d65e31b794fedcc50
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2021-01-14 11:27:03 +0800

    upload usart package

---
+++ usart/main.c
@@ -0,0 +1,88 @@
+#include <stm32f4xx.h>
+#include <stdio.h>
+
+void delay(uint32_t count)
+{
+	while(count--);
+}
+
+void usart3_init(void)
+{
+	/* RCC initialization */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+	/* GPIO initialization */
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
+
+	GPIO_InitTypeDef GPIO_InitStruct = {
+		.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11,
+		.GPIO_Mode = GPIO_Mode_AF,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_OType = GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_UP
+	};
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	/* USART initialization */
+	USART_InitTypeDef USART_InitStruct = {
+		.USART_BaudRate = 115200,
+		.USART_Mode = USART_Mode_Rx | USART_Mode_Tx,
+		.USART_WordLength = USART_WordLength_8b,
+		.USART_StopBits = USART_StopBits_1,
+		.USART_Parity = USART_Parity_No
+	};
+	USART_Init(USART3, &USART_InitStruct);
+
+	USART_Cmd(USART3, ENABLE);
+
+	USART_ClearFlag(USART3, USART_FLAG_TC);
+}
+
+char usart_getc(void)
+{
+	while(USART_GetFlagStatus(USART3, USART_FLAG_RXNE) != SET);
+	return USART_ReceiveData(USART3);
+}
+
+void usart_putc(char data)
+{
+	/* wait until TXE (Transmit Data Register Empty) flag is set */
+	while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+	USART_SendData(USART3, data);
+	/* wait until TC (Tranmission Complete) flag is set */
+	while(USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+}
+
+void usart_puts(char *string)
+{
+	for(; *string != '\0'; string++) {
+		usart_putc(*string);
+	}
+}
+
+int main()
+{
+	usart3_init();
+
+	usart_puts("\nSTM32: Hello World!\n\r");
+
+	int num = 0;
+
+	while(1) {
+		delay(10000000);
+		if (num >= 9){
+			num = 0;
+		}else
+		{
+			num++;
+		}
+
+		char char_arr[2];
+		sprintf(char_arr, "%d\n", num);
+		usart_puts(char_arr);
+	}
+
+	return 0;
+}
