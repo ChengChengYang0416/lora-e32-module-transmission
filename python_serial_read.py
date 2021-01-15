fc773e8bebefe71e23dc3d234f15cf95548da4e7
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2021-01-15 17:28:58 +0800

    use counter to accumulate the data received

--- python_serial_read.py
+++ python_serial_read.py
@@ -5,12 +5,19 @@ port = '/dev/ttyUSB0'
 baudrate = 115200
 ser = serial.Serial(port, baudrate)
 print("connected to: " + ser.portstr)
+counter = 0
 
 while True:
 	try:
 		# read data from serial port and decode
 		data = ser.readline().decode('utf-8')
-		print(data)
+		global counter
+		counter += 1
+		if data == 'end\n':
+			print("End of the transmission test.\n")
+			print(repr(counter-3) + " packages received.\n")
+		else:
+			print(data)
 	except Exception as error_messages:
 		# exception
 		print(error_messages)
